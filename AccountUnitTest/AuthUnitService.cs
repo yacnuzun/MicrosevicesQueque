6e1192f4032ea2b21edc5eb5fef09dea6af92bd9
yacnuzun <yacn.uzun@gmail.com>
2025-05-29 15:28:48 +0300

    test(account): add unit tests for AccountApi

---
+++ AccountUnitTest/AuthUnitService.cs
@@ -0,0 +1,87 @@
+﻿using AccountApi.Application.Services.Implementations;
+using AccountApi.Application.Services.Interfaces;
+using AccountApi.Domain.Entities;
+using AccountApi.Domain.Enums;
+using AccountApi.Dto_s;
+using AccountApi.Infrastructure.Helpers.JWT;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shared.Helpers.ResponseModels.GenericResultModels;
+using Shared.Helpers.Security.Hashing;
+
+namespace AccountUnitTest
+{
+    public class AuthUnitService
+    {
+        private readonly Mock<IUserService> _userServiceMock;
+        private readonly Mock<ITokenHelper> _tokenHelperMock;
+        private readonly Mock<ILogger<AuthManager>> _logmock;
+        private readonly AuthManager _authManagerMock;
+
+        public AuthUnitService()
+        {
+            _userServiceMock = new Mock<IUserService>();
+            _tokenHelperMock = new Mock<ITokenHelper>();
+            _logmock = new Mock<ILogger<AuthManager>>();
+            _authManagerMock = new AuthManager(_userServiceMock.Object,_tokenHelperMock.Object, _logmock.Object);
+        }
+
+        [Fact]
+        public async Task Register_ShouldReturnSuccess_WhenUserIsRegistered()
+        {
+            var registerDto = new UserForRegisterDto
+            {
+                Email = "asd@asd.com",
+                Password = "123456",
+                UserName = "TestUser",
+                Role    = UserRoles.Admin,
+                UserTaxId = "12345678910",
+            };
+            _userServiceMock.Setup(u=>u.Add(It.IsAny<User>(),registerDto.Role)).Verifiable();
+
+            var result = await _authManagerMock.Register(registerDto);
+
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Data.UserName.Should().Be("TestUser");
+            _userServiceMock.Verify(u=> u.Add(It.IsAny<User>(),registerDto.Role),Times.Once());
+        }
+        [Fact]
+        public async Task Login_shouldReturnSuccess_WhenUserIsLogin()
+        {
+            var loginDto = new UserForLoginDto
+            {
+                Password = "password",
+                UserName = "TestUser",
+                UserTaxID = "12345678910"
+            };
+
+            byte[] passwordHash, passwordSalt;
+
+            HashingHelper.CreatePasswordHash(loginDto.Password,out passwordHash,out passwordSalt);
+
+            var fakeUser = new User
+            {
+                UserTaxID = loginDto.UserTaxID,
+                UserName = loginDto.UserName,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Status = true
+
+            };
+
+            _userServiceMock.Setup(u => u.GetByUserTaxId(loginDto.UserTaxID)).ReturnsAsync(new SuccessDataResult<User>(fakeUser));
+
+            var result = await _authManagerMock.Login(loginDto);
+
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull();
+            result.Data.UserName.Should().Be("TestUser");
+
+
+            _userServiceMock.Verify(u=> u.GetByUserTaxId(loginDto.UserTaxID), Times.Once());
+        }
+    }
+}
---
+++ AccountUnitTest/UserUnitService.cs
@@ -0,0 +1,93 @@
+﻿using AccountApi.Application.Services.Implementations;
+using AccountApi.Application.Services.Interfaces;
+using AccountApi.Domain.Entities;
+using AccountApi.Infrastructure.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shared.Persistance.Interfaces;
+using System;
+using System.Linq.Expressions;
+
+namespace AccountUnitTest
+{
+    public class UserUnitService
+    {
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IUserOperationClaimService> _userOperationClaimServiceMock;
+        private readonly Mock<IOperationClaimService> _operationClaimServiceMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly Mock<ILogger<UserManager>> _loggerMock;
+        private readonly UserManager _userManager;
+
+        public UserUnitService()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _userOperationClaimServiceMock = new Mock<IUserOperationClaimService>();
+            _operationClaimServiceMock = new Mock<IOperationClaimService>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _loggerMock = new Mock<ILogger<UserManager>>();
+            _userManager = new UserManager(_userRepositoryMock.Object,_unitOfWorkMock.Object,_loggerMock.Object, _userOperationClaimServiceMock.Object, _operationClaimServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsSuccessDataResult_WhenUserExists()
+        {
+            // Arrange
+            int userId = 1;
+            var user = new User { Id = userId, Status = true };
+            _userRepositoryMock.Setup(repo => repo.GetAsync(u => u.Id == userId && u.Status == true))
+                .ReturnsAsync(user);
+
+            // Act
+            var result = await _userManager.GetById(userId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.Equal(userId, result.Data.Id);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsErrorDataResult_WhenUserDoesNotExist()
+        {
+            // Arrange
+            int userId = 1;
+            _userRepositoryMock.Setup(repo => repo.GetAsync(u => u.Id == userId && u.Status == true))
+                .ReturnsAsync((User)null);
+
+            // Act
+            var result = await _userManager.GetById(userId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.False(result.Success);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public async Task GetById_ThrowsException_LogsError()
+        {
+            // Arrange
+            int userId = 1;
+            var exception = new System.Exception("Test exception");
+            _userRepositoryMock.Setup(repo => repo.GetAsync(r=>r.Id == userId && r.Status == true))
+                .ThrowsAsync(exception);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<System.Exception>(() => _userManager.GetById(userId));
+            _userRepositoryMock.Verify(s=>s.GetAsync(u=> u.Id == userId && u.Status == true), Times.Once());
+            _loggerMock.Verify(
+                x => x.Log(
+                    LogLevel.Error,
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, t) =>
+                        v.ToString().Contains("Test exception")),
+                    It.IsAny<Exception>(),
+                    It.IsAny<Func<It.IsAnyType, Exception, string>>()
+                ),
+                Times.Once()
+            );
+        }
+
+    }
+}
